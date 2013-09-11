b718633e1757215b73b73d21e87ab9db534d8d3d
Chip Tol <briant@fiveq.com>
2013-09-11 17:48:24 -0400

    Stop support.rb from being automatically loaded.
    Taken from https://github.com/carr/phone/pull/23

--- lib/phonie.rb
+++ lib/phonie.rb
@@ -1,5 +1,5 @@
 require "phonie/version"
-require "phonie/support" unless defined? ActiveSupport
+require "support/support" unless defined? ActiveSupport
 require "phonie/phone"
 require "phonie/country"
 require "phonie/railties/validator"
